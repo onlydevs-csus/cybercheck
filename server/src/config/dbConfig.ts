d7a46bd6b941083ac495f53b94bca0d17f83c87a
Rafaela Lomboy <rafaela.lomboy@gmail.com>
2022-11-12 13:23:34 -0800

    made changes as suggested
    merge changes

--- server/src/config/dbConfig.ts
+++ server/src/config/dbConfig.ts
@@ -2,12 +2,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const passwordDb = {
+const incidentResponseDb = {
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT!),
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    ssl: true
 };
 
-export default passwordDb;
+export default incidentResponseDb;
