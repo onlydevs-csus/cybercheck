9a0f1714128c37f88cfc4ee7c0df3b8c2653caac
KeatonWakefield001 <keatonwakefield@csus.edu>
2022-11-16 11:40:22 -0800

    remved burger menu & placed manage account button

--- cyber-check/src/screens/RecentReports.tsx
+++ cyber-check/src/screens/RecentReports.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { StyleSheet, View, Text, Dimensions, Pressable } from "react-native";
+import { StyleSheet, View, Text, Dimensions, Pressable, TouchableOpacity } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "@rneui/base";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-
-import HamburgerMenu from '../components/HamburgerMenu';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 type RootStackParamList = {};
 
@@ -23,7 +23,6 @@ const Tab = createBottomTabNavigator();
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
-
 
 const RecentReportsTab = () => {
   const [searchPhrase, setSearchPhrase] = useState("");
@@ -82,95 +81,50 @@ const RecentReportsTab = () => {
   );
 };
 
-const DATA = [
-  {
-    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    title: "First Report",
-  },
-  {
-    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    title: "Second Report",
-  },
-  {
-    id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    title: "Third Report",
-  },
-  {
-    id: "3434af33-34a3-4332-aaaa-56257142ed32",
-    title: "Fourth Report",
-  },
-  {
-    id: "3434af33-34af-4332-aaaa-56257142ed32",
-    title: "Fifth Report",
-  },
-  {
-    id: "3434af33-3fa3-4332-aaaa-56257142ed32",
-    title: "Sixth Report",
-  },
-  {
-    id: "3434af33-34f3-4332-aaaa-56257142ed32",
-    title: "Seven Report",
-  },
-  {
-    id: "3434af33-34a3-43f2-aaaa-56257142ed32",
-    title: "Eight Report",
-  },
-  {
-    id: "3434af33-3fa3-4332-aafa-56257142ed32",
-    title: "Ninth Report",
-  },
-  {
-    id: "3434af33-34f3-4332-ffaa-56257142ed32",
-    title: "Tenth Report",
-  },
-  {
-    id: "3434af33-34a3-43f2-aaaa-56233142ed32",
-    title: "Eleventh Report",
-  },
-];
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-  },
-  item: {
-    backgroundColor: "#D3D3D3",
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    height: 100,
-    width: 100,
-  },
-  title: {
-    fontSize: 16,
-  },
-  row: {
-    flex: 1,
-    justifyContent: "space-around",
-  },
-});
-const RecentReportsScreen = () => {
-  return (
-    <><View style = {{flexDirection: "row"}}> 
-      <HamburgerMenu />
-      </View>
-      
-    <View style = {{flex: 6}}>
-        <Tab.Navigator>
-          <Tab.Screen  
-          name="Recent Reports" 
-          component={RecentReportsTab}  />
-        </Tab.Navigator>
-      </View></>
-    
-  );
-};
+const RecentReportsScreen = ({ navigation }: Props) => (
+  <><SafeAreaView style={[
+    styles.container]}>
+      <View style={styles.navigationBar}>
+        <View style={{flex: 1, justifyContent: "center", alignItems: "flex-start"}}> 
+          <TouchableOpacity onPress={() => navigation.navigate("ManageAccount")}>
+            <Icon name="settings" type="material"></Icon>
+          </TouchableOpacity>
+        </View>
+        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}> 
+          <Text style={{fontSize: 30}}> Recent Reports </Text>
+        </View> 
+        <View style={{flex: 1, alignItems: "flex-end", justifyContent: "center"}}>
+          <TouchableOpacity //onPress={() => navigation.navigate("RecentReportsTab")}
+          >
+            <Icon name="tab" type="material"> </Icon>
+          </TouchableOpacity>
+        </View>
+      </View> 
+
+    <View style={{ flex: 6 }}>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Recent Reports"
+          component={RecentReportsTab}
+          options={{
+            headerShown: false
+          }} />
+      </Tab.Navigator>
+    </View>
+    </SafeAreaView>
+</>
+
+);
 
 const styles = StyleSheet.create({
+  navigationBar:{
+    backgroundColor: "grey",
+    flex: .3,
+    padding: 20,
+    flexDirection: "row",
+  },
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    flex: 1
   },
   button: {
     backgroundColor: "#007AFF",
